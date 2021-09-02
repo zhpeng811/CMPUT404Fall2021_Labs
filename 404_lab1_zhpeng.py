ef26386d069d1e9328ba7110403ce3c5a4d6e799
Ze Hui Peng <zhpeng@ualberta.ca>
2021-09-01 21:28:40 -0600

    Add files via upload

---
+++ 404_lab1_zhpeng.py
@@ -0,0 +1,10 @@
+import requests
+
+print(requests.__version__)
+
+google_response = requests.get("http://google.com")
+print(google_response.status_code)
+
+# URL obtained by clicking the 'Raw' button on github
+github_response = requests.get("https://raw.githubusercontent.com/zhpeng811/CMPUT404Fall2021_Lab1/main/404_lab1_zhpeng.py")
+print(github_response.text)
